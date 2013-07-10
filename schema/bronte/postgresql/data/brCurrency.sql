29154355511cca5dcca7919a5df3a2df7bc4a600
Šimon Lukašík <isimluk@fedoraproject.org>
2013-07-10 02:14:35 +0200

    Insert some currencies and stock exchanges.

---
+++ schema/bronte/postgresql/data/brCurrency.sql
@@ -0,0 +1,30 @@
+--
+-- Copyright (c) 2013 Simon Lukasik
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+
+INSERT INTO brCurrency (id, code, sign, name) VALUES (036, 'AUD', NULL,    'Australian dollar');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (124, 'CAD', NULL,   'Canadian dollar');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (136, 'KYD', NULL,   'Cayman Islands dollar');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (156, 'CNY', 'CN¥',  'Chinese yuan');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (203, 'CZK', 'Kč',   'Czech koruna');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (344, 'HKD', NULL,   'Hong Kong dollar');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (348, 'HUF', NULL,   'Hungarian forint');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (376, 'ILS', '₪',    'Israeli new shekel');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (392, 'JPY', '¥',    'Japanese yen');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (578, 'NOK', NULL,   'Norwegian krone');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (643, 'RUB', NULL,   'Russian rouble');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (702, 'SGD', NULL,   'Singapore dollar');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (752, 'SEK',  NULL,  'Swedish krona');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (756, 'CHF', 'SFr.', 'Swiss franc');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (826, 'GBP', '£',    'Pound sterling');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (840, 'USD', '$',    'United States dollar');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (978, 'EUR', '€',    'Euro');
+INSERT INTO brCurrency (id, code, sign, name) VALUES (985, 'PLN', 'zł',   'Polish złoty');
+
---
+++ schema/bronte/postgresql/data/brStockMarket.sql
@@ -0,0 +1,31 @@
+--
+-- Copyright (c) 2013 Simon Lukasik
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'NYSE', 'New York Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'NASDAQ', 'National Association of Securities Dealers Automated Quotations');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'nasdaqNM', 'Nasdaq National Market Securities');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'LON', 'London Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'VIE', 'Vienna Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'TSX', 'Toronto Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'WSE', 'Warsaw Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'PSE', 'Prague Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'BSE', 'Budapest Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'FWB', 'Frankfurt Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'CSE', 'Copenhagen Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'TASE', 'Tel Aviv Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'ASX', 'Australian Securities Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'BME', 'Bolsas y Mercados Españoles');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'SGX', 'Singapore Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'TSEC', 'Taiwan Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'JSE', 'JSE Limited (Johannesburg)');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'SSE', 'Shanghai Stock Exchange');
+INSERT INTO brStockMarket (id, acronym, name) VALUES (sequence_nextval('br_stockmarker_id_seq'), 'SEHK', 'Hong Kong Stock Exchange');
+
